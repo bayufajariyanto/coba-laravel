6c693213fcbc9fd5ac7edb7dea7193d8668f83e4
Bayu Fajariyanto <bayuf08@gmail.com>
2022-02-28 20:39:21 +0700

    28 Februari 2022

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,8 @@
 
 @section('container')
     <h2 class="mb-5">{{ $post->title }}</h2>
+
+    <p>By. Bayu Fajariyanto in <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
     <a href="/posts">Back to Posts</a>
 @endsection
