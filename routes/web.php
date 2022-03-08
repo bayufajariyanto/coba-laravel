74056cbe91c7897917463a105d75162c6f0f8e96
Bayu Fajariyanto <bayuf08@gmail.com>
2022-03-08 23:31:48 +0700

    n+1 problems & clockwork

--- routes/web.php
+++ routes/web.php
@@ -35,23 +35,22 @@ Route::get('/about', function () {
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/categories/', function(){
+Route::get('/categories', function(){
     return view('categories', [
         'title' => 'Post Categories',
         'categories' => Category::all()
     ]);
 });
 Route::get('/categories/{category:slug}', function(Category $category){
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
+    return view('posts', [
+        'title' => "Post by Category : $category->name",
+        'posts' => $category->posts->load('category', 'author')
     ]);
 });
 
 Route::get('/authors/{author:username}', function(User $author){
     return view('posts', [
-        'title' => 'User Posts',
-        'posts' => $author->posts
+        'title' => "Post by Author : $author->name",
+        'posts' => $author->posts->load('category', 'author')
     ]);
 });
